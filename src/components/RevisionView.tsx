0d8eb53ff1fce6d1895803716c9a07f72db9ad5e
Phill <50775241+Phill030@users.noreply.github.com>
2024-04-19 16:00:35 +0200

    feat: add `SelecteddItem` to `RevisionView`

--- src/components/RevisionView.tsx
+++ src/components/RevisionView.tsx
@@ -1,10 +1,16 @@
 import "../scss/RevisionView.scss";
+import { useState } from "react";
 import FileList from "./FileList";
+import SelectedItem from "./Selecteditem";
+import { File } from "../interfaces/File";
 
 export default function RevisionView({ revision }: { revision: string }) {
+  let [file, setFile] = useState<File>();
+
   return (
     <div className="revision-view">
-      <FileList revision={revision} />
+      <FileList revision={revision} onFileClick={(f: File) => setFile(f)} />
+      <SelectedItem file={file} />
     </div>
   );
 }
