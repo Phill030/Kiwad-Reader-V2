488e5bfefe6241e457ee34377477a5a43aa153b7
Phill <50775241+Phill030@users.noreply.github.com>
2024-04-19 12:48:15 +0200

    chore: remove unused imports

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::collections::VecDeque;
-
 use downloader::fetch_revisions;
-use fetcher::{Fetcher, File, LatestFiles};
-use tauri::{generate_handler, Window};
+use fetcher::Fetcher;
+use tauri::generate_handler;
 
 pub mod downloader;
 pub mod fetcher;
