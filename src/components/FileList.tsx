b9f831f6eacd83e0484756a1fecd82507e51beed
Phill <50775241+Phill030@users.noreply.github.com>
2024-04-19 19:28:08 +0200

    ref: move file fetching to `RevisionView`

--- src/components/FileList.tsx
+++ src/components/FileList.tsx
@@ -1,73 +1,26 @@
 import {
-  Classes,
-  Icon,
-  Intent,
   Spinner,
   Tree,
-  TreeNodeInfo,
 } from "@blueprintjs/core";
 import "../scss/FileList.scss";
-import { invoke } from "@tauri-apps/api";
 import { File } from "../interfaces/File";
-import { useEffect, useState } from "react";
+
 
 export default function FileList(props: any) {
-  let [tree, setTree] = useState<TreeNodeInfo[]>([]);
-  let [files, setFiles] = useState<File[]>([]);
-
-  useEffect(() => {
-    async function getFiles(revision: String): Promise<void> {
-      const files = await invoke<string>("fetch_files", { revision });
-      let parsed: File[] = JSON.parse(files);
-
-      let treeNodes: TreeNodeInfo[] = [];
-      parsed.forEach((v, idx) => {
-        treeNodes.push({
-          id: idx + 1,
-          label: `${v.filename}`,
-          icon: (
-            <Icon
-              icon="compressed"
-              intent={Intent.PRIMARY}
-              className={Classes.TREE_NODE_ICON}
-            />
-          ),
-        });
-      });
-
-      setTree(treeNodes);
-      setFiles(parsed);
-    }
-
-    getFiles(props.revision);
-  }, []);
-
-  const handleNodeExpand = async (node: any, nodePath: any) => {
-    console.log(node);
-    console.log(nodePath);
-  };
-
   const handleNodeClick = (node: any, nodePath: any) => {
     console.log(node);
     console.log(nodePath);
 
-    let file = files.find(f => f.filename.replace("Data/GameData/", "").replace(".wad", "") == node.label);
+    let file = props.files.find((f: File) => f.filename.replace("Data/GameData/", "").replace(".wad", "") == node.label);
     props.onFileClick(file);
-  };
-
-  const handleNodeCollapse = (node: any, nodePath: any) => {
-    console.log(node);
-    console.log(nodePath);
   };
 
   return (
     <div className="file-list">
-      {tree.length > 0 ? (
+      {props.tree.length > 0 ? (
         <Tree
-          contents={tree}
+          contents={props.tree}
           onNodeClick={handleNodeClick}
-          onNodeCollapse={handleNodeCollapse}
-          onNodeExpand={handleNodeExpand}
         ></Tree>
       ) : (
         <Spinner />
--- src/components/RevisionView.tsx
+++ src/components/RevisionView.tsx
@@ -1,16 +1,56 @@
 import "../scss/RevisionView.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import FileList from "./FileList";
-import SelectedItem from "./Selecteditem";
+import SelectedItem from "./SelectedItem";
 import { File } from "../interfaces/File";
+import { Classes, Icon, Intent, TreeNodeInfo } from "@blueprintjs/core";
+import { invoke } from "@tauri-apps/api";
 
 export default function RevisionView({ revision }: { revision: string }) {
+  let [tree, setTree] = useState<TreeNodeInfo[]>([]);
+  let [files, setFiles] = useState<File[]>([]);
   let [file, setFile] = useState<File>();
+
+  useEffect(() => {
+    async function getFiles(revision: String): Promise<void> {
+      const files = await invoke<string>("fetch_files", { revision });
+      let parsed: File[] = JSON.parse(files);
+
+      let treeNodes: TreeNodeInfo[] = [];
+      parsed.forEach((v, idx) => {
+        treeNodes.push({
+          id: idx + 1,
+          label: `${v.filename}`,
+          icon: (
+            <Icon
+              icon="compressed"
+              intent={Intent.PRIMARY}
+              className={Classes.TREE_NODE_ICON}
+            />
+          ),
+        });
+      });
+
+      setFiles(parsed);
+      setTree(treeNodes);
+    }
+
+    getFiles(revision);
+  }, []);
 
   return (
     <div className="revision-view">
-      <FileList revision={revision} onFileClick={(f: File) => setFile(f)} />
-      <SelectedItem file={file} />
+      <div className="left-content">
+        <FileList
+          revision={revision}
+          onFileClick={(f: File) => setFile(f)}
+          tree={tree}
+          files={files}
+        />
+      </div>
+      <div className="right-content">
+        <SelectedItem revision={revision} file={file} />
+      </div>
     </div>
   );
 }
